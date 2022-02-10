d253ed772bfb45081bcc342dd6e2c48d1e830b57
JoeCSykes <91789402+JoeCSykes@users.noreply.github.com>
2022-02-10 16:36:05 +0000

    Delete python_assessment directory

--- python_assessment/api/run_api.bat
+++
@@ -1,9 +0,0 @@
-echo Installing Pipenv
-pip install pipenv
-
-echo Installing from Pipfile
-pipenv install
-
-echo Activating Python Virtual Environment
-echo Running Flask API
-pipenv run flask run
--- python_assessment/src/interfaces.ts
+++
@@ -1,7 +0,0 @@
-export interface IPerson {
-  id: number
-  firstName: string
-  lastName: string
-  authorised: boolean
-  enabled: boolean
-}
